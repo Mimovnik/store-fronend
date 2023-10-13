ce70198b78c7ca1e07a74ed9e483e7ff2d964c68
mimovnik <jkwidzinski@gmail.com>
2023-10-13 14:03:15 +0200

    Remove privacy policy from navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,7 +7,6 @@ const Navbar = () => {
       <div className="links">
         <Link to="/">Home</Link>
         <Link to="/products">Products</Link>
-        <Link to="/privacy-policy">Privacy policy</Link>
       </div>
     </nav>
   );
